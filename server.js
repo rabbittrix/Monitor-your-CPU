e51301d9b8aa43e69035d300209a5eec0a40e5da
Roberto de Souza <rabbittrix@hotmail.com>
2019-04-24 13:55:41 +0100

    Monitoramento de CPU

---
+++ server.js
@@ -0,0 +1,19 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var cpu = require('./cpu.js');
+var interval = 1000;
+ 
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+ 
+setInterval(function(){
+    cpu.getPercentageUsage(interval, function(percentage){
+        io.emit('cpu_usage', percentage);
+    });
+}, interval);
+ 
+http.listen(3000, function(){
+  console.log('Listening on *:3000');
+});
